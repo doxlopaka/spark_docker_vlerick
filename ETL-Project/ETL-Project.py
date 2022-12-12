ae9143f7bd21a2cb115cb4facd5d4cf756303b41
doxlopaka <ahmadfouadfleifel@gmail.com>
2022-12-12 17:10:06 +0000

    updated py file

--- ETL-Project/ETL-Project.py
+++ ETL-Project/ETL-Project.py
@@ -1,9 +1,9 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
-BUCKET = 
-KEY1 = 
-KEY2 = 
+BUCKET = "dmacademy-course-assets" 
+KEY1 = "vlerick/after_release.csv"
+KEY2 = "vlerick/pre_release.csv"
 
 config = {"spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
           "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.f3.s3a.InstanceProfileCredentialsProvider",
