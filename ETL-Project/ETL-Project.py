fb7774e7e80e6c25eefa870b778fcadbba021b9b
doxlopaka <ahmadfouadfleifel@gmail.com>
2022-12-13 15:14:49 +0000

    configured credentials

--- ETL-Project/ETL-Project.py
+++ ETL-Project/ETL-Project.py
@@ -1,3 +1,4 @@
+import os
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
@@ -10,14 +11,14 @@ BUCKET = "dmacademy-course-assets"
 KEY1 = "vlerick/after_release.csv"
 KEY2 = "vlerick/pre_release.csv"
 
-config = {"spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1"
-}
+if len(os.environ.get("AWS_SECRET_ACCESS_KEY")) < 1:
 
-'''
-config = {"spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
-          "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.f3.s3a.SimpleAWSCredentialsProvider"
-}
-'''
+    config = {"spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
+          "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.f3.s3a.InstanceProfileCredentialsProvider"
+    }
+else:
+    config = {"spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1"
+    }
 
 
 conf = SparkConf().setAll(config.items())
